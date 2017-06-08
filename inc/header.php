3f1ade1b482c4518949138915e399d11b92785bd
Jason Brown <jasontbrown99@gmail.com>
2017-06-08 11:43:57 -0700

    set up initial files with jquery ui working

---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?php if(isset($title)){ echo $title; }?></title>
+
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="jquery_ui/css/jquery-ui.min.css" />
+    <script type="text/javascript" src="jquery_ui/js/jquery.js" defer></script>
+    <script type="text/javascript" src="jquery_ui/js/jquery-ui.js" defer></script>
+    <script type="text/javascript" src="jquery_ui/js/main.js" defer></script>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<?php echo "hello??" ?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$title = 'Thursday Things';
+//include header template
+require('inc/header.php');
+?>
+<div class="container">
+    <div class="">
+    </div>
+    <input type="text" name="date" id="date">
+</div>
+<?php
+//include header template
+require('inc/footer.php');
+?>
